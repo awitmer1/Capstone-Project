08d4d20fb342e4da16a2633c8e1d15a47f4661ab
Andrew Witmer <witmer.a@gmail.com>
2023-07-27 10:21:23 -0700

    Added url.py to apps

---
+++ backend/comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from comment import views
+
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
+# urlpatterns = [
+# path('<str:video_id>', views.get_all_comments_by_id),
+# path('postto/', views.user_comments),
---
+++ backend/saved_places/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from saved_places import views
+
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
+# urlpatterns = [
+# path('<str:video_id>', views.get_all_comments_by_id),
+# path('postto/', views.user_comments),

1d1a1dda2a57116563caeded162c578c2bc3455b
Andrew Witmer <witmer.a@gmail.com>
2023-07-30 16:35:16 -0700

    Added initial mapping of search results (needs additional work [functionality, styling])

--- frontend/src/pages/ResultsPage/ResultsPage.js
+++ frontend/src/pages/ResultsPage/ResultsPage.js
@@ -1,6 +1,8 @@
 // General Imports
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
+import axios from "axios";
+import { KEY } from "../../localKey";
 
 // Component Imports
 
@@ -9,11 +11,42 @@ import SearchContext from "../../context/SearchContext";
 
 const ResultsPage = () => {
   const { search, setSearch } = useContext(SearchContext);
+  const [initRests, setInitRests] = useState([]);
+
+  async function fetchRestaurants(search) {
+    const response = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${search}&term=dog%20friendly&limit=3&sort_by=best_match&categories=restaurants";`,
+      {
+        headers: { Authorization: `Bearer ${KEY}`, accept: `application/json` },
+      }
+    );
+    console.log("User search results");
+    console.log(response.data.businesses);
+    setInitRests(response.data.businesses);
+  }
+
+  useEffect(() => {
+    fetchRestaurants(search);
+  }, []);
 
   return (
-  <>
-  <p>{search}</p>
-  </>
+    <>
+      <p>Results for: {search}</p>
+      <div>
+        <h3>Restaurants</h3>
+        {console.log("initRests check for mapping")}
+        {console.log(initRests)}
+        {initRests &&
+          initRests.map((item) => {
+            return (
+              <>
+                <p>{item.name}</p>
+                <p style={{ color: "grey" }}>{item.categories[0].title}</p>
+              </>
+            );
+          })}
+      </div>
+    </>
   );
 };
 

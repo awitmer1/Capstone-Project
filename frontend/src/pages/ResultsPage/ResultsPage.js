ed8eb9107d85b81d1f4517f710bd25a9a7750e8f
Andrew Witmer <witmer.a@gmail.com>
2023-07-31 09:48:13 -0700

    Added additional categories on main search page (dog parks + public parks)

--- frontend/src/pages/ResultsPage/ResultsPage.js
+++ frontend/src/pages/ResultsPage/ResultsPage.js
@@ -13,6 +13,8 @@ import SearchContext from "../../context/SearchContext";
 const ResultsPage = () => {
   const { search, setSearch } = useContext(SearchContext);
   const [initRests, setInitRests] = useState([]);
+  const [initDogParks, setInitDogParks] = useState([]);
+  const [initPubParks, setInitPubParks] = useState([]);
 
   async function fetchRestaurants(search) {
     try {
@@ -33,16 +35,65 @@ const ResultsPage = () => {
     }
   }
 
+  async function fetchDogParks(search) {
+    try {
+      const response = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${search}&limit=3&sort_by=best_match&term=dog_parks";`,
+        {
+          headers: {
+            Authorization: `Bearer ${KEY}`,
+            accept: `application/json`,
+          },
+        }
+      );
+      // console.log("User search results");
+      // console.log(response.data.businesses);
+      setInitDogParks(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchPubParks(search) {
+    try {
+      const response = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${search}&limit=3&sort_by=best_match&term=parks";`,
+        {
+          headers: {
+            Authorization: `Bearer ${KEY}`,
+            accept: `application/json`,
+          },
+        }
+      );
+      // console.log("User search results");
+      // console.log(response.data.businesses);
+      setInitPubParks(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   useEffect(() => {
     fetchRestaurants(search);
+    fetchDogParks(search);
+    fetchPubParks(search);
   }, []);
 
   return (
     <>
       <p>Results for: {search}</p>
       <div className="restaurants-main">
-        <h3>Restaurants</h3>
+        <h3>Restaurants & Bars</h3>
         <ResultsCards inputs={initRests} />
+      </div>
+      <div className="dog-parks-main">
+        <h3>Dog Parks</h3>
+        <ResultsCards inputs={initDogParks} />
+      </div>
+      <div className="public-parks-main">
+        <h3>Public Parks</h3>
+        <ResultsCards inputs={initPubParks} />
       </div>
     </>
   );

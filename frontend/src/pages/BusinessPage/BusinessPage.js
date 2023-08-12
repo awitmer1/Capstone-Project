e9936167d17f8e6a3c23b80d8e6366e8bda557d4
Andrew Witmer <witmer.a@gmail.com>
2023-08-11 17:17:50 -0700

    Stored get response data in state; display business information on page (wip)

--- frontend/src/pages/BusinessPage/BusinessPage.js
+++ frontend/src/pages/BusinessPage/BusinessPage.js
@@ -1,6 +1,7 @@
 // General Imports
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { KEY } from "../../localKey";
 import { Link } from "react-router-dom";
@@ -14,24 +15,52 @@ import SearchContext from "../../context/SearchContext";
 import CategoryContext from "../../context/CategoryContext";
 
 const BusinessPage = () => {
+  // State Variables
+  const [bizData, setBizData] = useState([]);
+
   // Context Variables
   const { search, setSearch } = useContext(SearchContext);
   const { category, setCategory } = useContext(CategoryContext);
+  const { id } = useParams();
+
+  async function getBusinessInfo(id) {
+    try {
+      const response = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${KEY}`,
+            accept: `application/json`,
+          },
+        }
+      );
+      console.log("Business Search Result");
+      console.log(response.data);
+      setBizData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getBusinessInfo(id);
+  }, []);
 
   return (
     <div className='business-main'>
-      <div className='back-to-main'>
+      <div className='back-to-results'>
         <Link to={`/results`}>
           <p>Back</p>
         </Link>
       </div>
       <div>
-        <h3>Business Name</h3>
-        <p>Category</p>
+        <h3>{bizData.name}</h3>
+        <p>{bizData.display_phone}</p>
+        <p>{bizData.categories.title}</p>
         <p>Description</p>
         <p>Hours</p>
         <p>Location</p>
-        <p>Phone</p>
+        <a href={`${bizData.url}`}>Link to Yelp Page: </a>
       </div>
     </div>
   );
